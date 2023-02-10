3156c7e0e217de6e23d652feda57740f60a57196
Elvin Agakerimov <shipagakerimov@mail.ru>
2023-02-11 03:48:48 +0500

    adding quotes to object properties

--- codeWars-lvl/110223.js
+++ codeWars-lvl/110223.js
@@ -1,12 +1,12 @@
 function phoneticLookup(val) {
   let result = ""
   var lookup =  {
-      'alpha' : "Adams",
-      'bravo': "Boston",
-      'charlie': "Chicago",
-      'delta': "Denver",
-      'echo': "Easy",
-      'foxtrot': "Frank",
+      "alpha" : "Adams",
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank",
     }
     result = lookup[val];
     return result 
